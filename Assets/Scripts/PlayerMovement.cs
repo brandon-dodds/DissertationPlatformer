480534b525e8e7498efbd55d8a830a307ad2bbfe
Brandon <pokemonbrandon1234@gmail.com>
2021-12-13 10:54:54 +0000

    Changed from a rigidbody movement to a translate movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
-    // Update is called once per frame
-    private Rigidbody2D _rigidBody2d;
-
-    private void Start()
-    {
-        // Cache the value, don't call GetComponent<Rigidbody2D>() in FixedUpdate
-        _rigidBody2d = GetComponent<Rigidbody2D>();
-    }
     void Update()
     {
-        _rigidBody2d.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
     } 
 }
