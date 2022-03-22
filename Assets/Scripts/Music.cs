c9f8867e7c3f1f58235865ca341814a3f86316bb
Brandon <pokemonbrandon1234@gmail.com>
2022-03-22 12:48:22 +0000

    Changed it to a "how long standing" system.

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -7,6 +7,7 @@ public class Music : MonoBehaviour
     [SerializeField] AudioMixer PercussionAudio;
     [SerializeField] PlayerMovement playerMovement;
     float volume;
+    float playerStandStill;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,14 @@ public class Music : MonoBehaviour
     {
         PercussionAudio.GetFloat("PercussionVolume", out volume);
         if (playerMovement.isPlayerMoving && volume != 0.0f){
+            playerStandStill = 0;
             PercussionAudio.SetFloat("PercussionVolume", 0.0f);
         }
-        if (!playerMovement.isPlayerMoving && volume != -80.0f)
+        if (!playerMovement.isPlayerMoving)
+        {
+            playerStandStill += Time.deltaTime;
+        }
+        if (playerStandStill > 0.2f && volume != -80.0f)
         {
             PercussionAudio.SetFloat("PercussionVolume", -80.0f);
         }
