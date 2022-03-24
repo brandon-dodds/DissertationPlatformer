578a3f932bb223330e2cb1177f9095c1a7b12265
Brandon <pokemonbrandon1234@gmail.com>
2022-03-24 12:57:34 +0000

    Added code to change dynamic and static types.

---
+++ Assets/Scripts/MusicChange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class MusicChange : MonoBehaviour
+{
+    [SerializeField] AudioMixer dynamicSound;
+    [SerializeField] AudioMixer staticSound;
+    float dynamicVolume;
+    float staticVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dynamicSound.SetFloat("DynamicVolume", 0.0f);
+        staticSound.SetFloat("StaticVolume", -80.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dynamicSound.GetFloat("DynamicVolume", out dynamicVolume);
+        staticSound.GetFloat("StaticVolume", out staticVolume);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(dynamicVolume == 0.0f)
+            {
+                dynamicSound.SetFloat("DynamicVolume", -80.0f);
+            }
+            else
+            {
+                dynamicSound.SetFloat("DynamicVolume", 0.0f);
+            }
+
+            if (staticVolume == 0.0f)
+            {
+                staticSound.SetFloat("StaticVolume", -80.0f);
+            }
+            else
+            {
+                staticSound.SetFloat("StaticVolume", 0.0f);
+            }
+        }
+    }
+}
